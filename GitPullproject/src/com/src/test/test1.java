9a1f5ee128ef535455db9d34a98618b5cd9afde3
shekharsing <58291594+shekharsing@users.noreply.github.com>
2019-11-29 13:44:36 +0530

    Update test1.java

--- GitPullproject/src/com/src/test/test1.java
+++ GitPullproject/src/com/src/test/test1.java
@@ -3,7 +3,7 @@ package com.src.test;
 public class test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Response Body is");
+		System.out.println("Response Body is updated");
 
 	}
 
